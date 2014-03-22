b823e1d5bc0bf8b3d223e231b0c612fd497158bf
joshfermin <joshfermin@gmail.com>
2014-03-22 15:22:42 -0600

    Created a basic User model (included secure passwords)

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '1.0.2'
 gem 'atomic', '1.1.16'
 gem 'sprockets', '2.11.0'
+gem 'bcrypt-ruby', '3.1.2' # for password hash
 
 group :doc do
   gem 'sdoc', '0.3.20', require: false
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
-end
+  has_secure_password
+
+  before_save { self.email = email.downcase }
+
+  validates :name, presence: true, length: { maximum: 50 } # verifying that name is present
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            format: { with: VALID_EMAIL_REGEX},
+            uniqueness: { case_sensitive: false }
+    # uniqueness doesn't fully ensure uniqueness: i.e user hits submit button twice
+
+  validates :password, presence: true, length: {minimum: 6}
+  end
