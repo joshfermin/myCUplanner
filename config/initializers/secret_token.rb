6b0f4eedb6f91c8e7e8813e828d740e2107fc614
joshfermin <joshfermin@gmail.com>
2014-03-22 10:30:27 -0600

    initialize default app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyCUplanner::Application.config.secret_key_base = '828ff4b0966ade895f20daa7303acf293dd9ce7e0fdbf7ba40c0cebd87d4f45b2c457714b31df92d6c20a38c5678c16c4a99a766fe59ed491c711bdbfe7be497'
