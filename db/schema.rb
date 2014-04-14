553250dff42279884855d77e01b05b0271c1319b
joshfermin <joshfermin@gmail.com>
2014-04-14 16:23:14 -0600

    fixes gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20140413213402) do
 
   create_table "courses", force: true do |t|
@@ -23,9 +22,6 @@ ActiveRecord::Schema.define(version: 20140413213402) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-
-ActiveRecord::Schema.define(version: 20140409021537) do
-
 
   create_table "users", force: true do |t|
     t.string   "name"
