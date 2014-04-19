1e1222844dd0f3e1b71bfd99bb46773f28eb3019
joshfermin <joshfermin@gmail.com>
2014-04-19 16:04:43 -0600

    cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140419184823) do
+ActiveRecord::Schema.define(version: 20140419213420) do
 
   create_table "courses", force: true do |t|
     t.string   "course_number"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20140419184823) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
-    t.string   "courses_taken"
+    t.text     "courses_taken"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
