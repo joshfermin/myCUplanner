d2b2b307642765b042323ff34e82b927ee4e7858
deGonzz <louis.bouddhou@gmail.com>
2014-04-19 13:43:33 -0600

    Adds all classes from next semester into database.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+# FasterCSV.foreach("RAILS.root/Schedules.csv") do |row|
+
+CSV.foreach("Schedules.csv") do |row|
+	Course.create(
+	:course_number => row[0],
+	:course_title => row[1],
+	:room => row[2],
+	:instructor => row[3],
+	:time => row[4],
+	)
+end
