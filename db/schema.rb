4fb4b32cb7c4e0a7b247e7c2edf8fbb2efbc6ac0
joshfermin <joshfermin@gmail.com>
2014-04-24 17:26:23 -0600

    database cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140423210032) do
+ActiveRecord::Schema.define(version: 20140423224025) do
 
   create_table "calendars", force: true do |t|
     t.datetime "created_at"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20140423210032) do
     t.string   "room"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
 
   create_table "events", force: true do |t|
